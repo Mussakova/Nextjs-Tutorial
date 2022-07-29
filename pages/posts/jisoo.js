e08f69491064a33b4bc89c67d4cdb9a8f9dca39a
Hirepan E <ginebrasyrel@gmail.com>
2022-07-28 19:27:02 -0500

    agregando linter y pagina de Jisoo

---
+++ pages/posts/jisoo.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+
+export default function Jisoo() {
+  return (
+    <>
+      <Head>
+        <title>Kim Jisoo</title>
+      </Head>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={400} // Desired size with correct aspect ratio
+        width={400} // Desired size with correct aspect ratio
+        alt="Kim Jisoo" // Alt text to be read by screen readers
+      />
+      <h1>Jisoo</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
