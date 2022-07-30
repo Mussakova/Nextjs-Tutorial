a92b786ab1d670eb203d294b71da3abf55b8e1d8
Hirepan E <ginebrasyrel@gmail.com>
2022-07-29 21:43:03 -0500

    Corrigiendo ruta de img Jisoo

--- pages/posts/jisoo.js
+++ pages/posts/jisoo.js
@@ -9,12 +9,12 @@ export default function Jisoo() {
         <title>Kim Jisoo</title>
       </Head>
       <Image
-        src="/images/profile.jpg" // Route of the image file
+        src="/images/jisoo.jpg" // Route of the image file
         height={400} // Desired size with correct aspect ratio
         width={400} // Desired size with correct aspect ratio
         alt="Kim Jisoo" // Alt text to be read by screen readers
       />
-      <h1>Jisoo</h1>
+      <h1>Kim Jisoo</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
